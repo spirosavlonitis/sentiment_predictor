7a899d7eb4258ae63355dc63aa1342ba95f5964b
Spiros Avlonitis <spirosa84@hotmail.com>
2018-12-14 05:50:43 +0200

    updated model

--- update.py
+++ update.py
@@ -1,17 +1,17 @@
 import numpy as np
 import sqlite3
-import pickle
+import pickle as pkl
 from vectorizer import vect
 from os import path
 
-def update_model():
+def update_model(batch_size=10000):
     """Update model from data stored in review_db."""
     
-    clf = pickle.load(
+    clf = pkl.load(
             open(path.join('pkl_objects', 'classifier.pkl'), 'rb')
         )
 
-    conn = sqlite3.connect('reviews')
+    conn = sqlite3.connect('reviews.sqlite')
     c = conn.cursor()
     c.execute("SELECT * FROM review_db")
 
@@ -22,7 +22,7 @@ def update_model():
         y = data[:, 1].astype(int)
         
         classes = np.array([0, 1])
-        X_train = vect.transfrom(X)
+        X_train = vect.transform(X)
         clf.partial_fit(X_train, y, classes=classes)
         results = c.fetchmany(batch_size)
     conn.close()
@@ -31,3 +31,7 @@ def update_model():
             protocol=4
         )
     return None
+
+
+if __name__ == '__main__':
+    update_model()
